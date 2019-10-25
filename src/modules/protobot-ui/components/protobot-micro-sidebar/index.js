a940e313b57cc60db3bd086cbc9b89810f5ec531
yoonseo <yoon0u0@gmail.com>
2019-10-25 15:09:49 +0900

    build: build protobot-micro-sidebar to modify

---
+++ src/modules/protobot-ui/components/protobot-micro-sidebar/index.js
@@ -0,0 +1,23 @@
+// Import the LitElement base class and html helper function
+import { LitElement, customElement } from 'lit-element';
+import { template } from './template.js';
+import { GetDomainMixin } from '../../mixins/get-domain';
+import { database } from '../../../firebase';
+
+// Extend the LitElement base class
+// @ts-ignore
+@customElement('protobot-micro-sidebar')
+class ProtobotMicroSidebar extends GetDomainMixin(LitElement) {
+  render () {
+    return template(this);
+  }
+
+  async deploy () {
+    const updates = {};
+    updates[`last-deployed/data/${this.domainId}/`] = this.domain;
+    updates[`domains/data/${this.domainId}/deployed`] = false;
+    await database.ref().update(updates);
+  }
+}
+
+export { ProtobotMicroSidebar };

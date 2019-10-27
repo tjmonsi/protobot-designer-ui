55e201390ec4eae451754fdbcd2528c4d486cd43
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2019-10-26 17:07:25 -0700

    chore: pull from yoonseo

--- src/modules/protobot-ui/components/protobot-micro/template.js
+++ src/modules/protobot-ui/components/protobot-micro/template.js
@@ -25,57 +25,6 @@ export const template = self => function () {
     <h1>Micro Review</h1>
     <h3>Crowd name: ${crowdID}</h3>
     <br>
-<<<<<<< HEAD
-    <div class="feed feed__right">
-      <div>
-        <div class="label">User</div>
-        <vaadin-button
-          theme= "primary"
-          class = "user-say"> User said!
-        </vaadin-button>
-        <div class="button-container button-container__right">
-          <vaadin-select class="topic-select">
-              <template>
-                <vaadin-list-box>
-                  <!-- wanna put topic here -->
-                  <vaadin-item>Jose</vaadin-item>
-                  <vaadin-item>Manolo</vaadin-item>
-                  <vaadin-item>Pedro</vaadin-item>
-                </vaadin-list-box>
-              </template>
-            </vaadin-select>
-        </div>
-      </div>
-    </div>
-    <br>
-    <div class="feed">
-      <div>
-        <div class="label">Bot</div>
-        <!-- ${topics.map(item => html`${item}`)} -->
-        <vaadin-button
-          theme= "contrast primary"
-          class= "bot-say"> Bot said!
-        </vaadin-button>
-        <div class="select-container">
-          <vaadin-select class="topic-select">
-            <template>
-              <vaadin-list-box>
-                <!-- wanna put topic here -->
-                  ${["topic1", "topic2"].map(item => html`<vaadin-item>${item}</vaadin-item>`)}
-                </vaadin-item>
-                <vaadin-item class="new-item-input">Add new topic</vaadin-item>
-              </vaadin-list-box>
-              <!-- <vaadin-list-box>
-                ${["topic4", "topic5"].map(item=> html`${item}`)}
-              </vaadin-list-box> -->
-            </template>
-          </vaadin-select>
-        </div>
-      </div>
-    </div>
-
-=======
->>>>>>> 60e65de98a1d84eec0acebc3b8c697c2b0665c16
 
     ${utterances && utterances.length ? utterances.map(item => html`
       <utterance-review-item .utteranceId="${item.id}"></utterance-review-item>
